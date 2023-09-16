56e969b79fb5f1873157586f14d221f390a605b0
kamote-kid <kamote-kid@gmail.com>
2023-09-16 15:23:15 +0800

    bugfixed: changed primary color not working

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="p-4">
-      <Button>This is the button component</Button>
+      <Button>Button</Button>
     </main>
   );
 }
