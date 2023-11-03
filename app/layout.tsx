654d639b73e4239217a6ea58d8599d6f34173ec4
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-11-03 20:30:39 +0800

    able to display the current user and logout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import ReactQueryProvider from '@/provider/ReactQueryProvider';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import AuthProvider from '@/provider/AuthProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
     return (
         <html lang='en'>
             <body className={inter.className}>
-                <ReactQueryProvider>{children}</ReactQueryProvider>
+                <ReactQueryProvider>
+                    <AuthProvider>{children}</AuthProvider>
+                </ReactQueryProvider>
             </body>
         </html>
     );
