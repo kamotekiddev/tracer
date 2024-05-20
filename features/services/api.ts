9df58e3cae97f70c9c0eb7437e9dd4b72a5f2dfa
kamotedev <kamotekiddev@gmail.com>
2024-05-20 20:03:00 +0800

    [CONFIG] Use axios interceptors to append token on every request

--- features/services/api.ts
+++ features/services/api.ts
@@ -1,5 +1,15 @@
+import Cookies from "js-cookie";
 import axios from "axios";
 
 const client = axios.create({ baseURL: process.env.NEXT_PUBLIC_BASE_URL });
+
+client.interceptors.request.use(
+    (config) => {
+        const token = Cookies.get("bearer");
+        if (token) config.headers.Authorization = `Bearer ${token}`;
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 export default client;
