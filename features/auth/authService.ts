206318369a27daa023bdfae398137ea44d78a9cb
kamotedev <kamotekiddev@gmail.com>
2024-05-21 21:07:04 +0800

    [CONFIG] auto refresh token if expired

--- features/auth/authService.ts
+++ features/auth/authService.ts
@@ -3,3 +3,6 @@ import { LoginSchemaType } from "./login/validation";
 
 export const login = (credential: LoginSchemaType) =>
     client.post("/auth/login", credential);
+
+export const refresh = (refreshToken: string) =>
+    client.patch("/auth/refresh", { refreshToken });
--- features/services/api.ts
+++ features/services/api.ts
@@ -1,13 +1,44 @@
+import { fromUnixTime, isBefore } from "date-fns";
+import axios, { AxiosResponse } from "axios";
 import Cookies from "js-cookie";
-import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
-const client = axios.create({ baseURL: process.env.NEXT_PUBLIC_BASE_URL });
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+const client = axios.create({ baseURL });
+
+const refreshToken = async (refreshToken: string) => {
+    const response: AxiosResponse<{
+        accessToken: string;
+    }> = await axios.patch(`${baseURL}/auth/refresh`, {
+        refreshToken,
+    });
+
+    const accessToken = response.data.accessToken;
+    Cookies.set("bearer", response.data.accessToken);
+
+    return accessToken;
+};
 
 client.interceptors.request.use(
-    (config) => {
+    async (req) => {
         const token = Cookies.get("bearer");
-        if (token) config.headers.Authorization = `Bearer ${token}`;
-        return config;
+        const refresh = Cookies.get("refresh");
+
+        if (!token) return req;
+        req.headers.Authorization = `Bearer ${token}`;
+
+        if (!refresh) return req;
+
+        const { exp } = jwtDecode(token);
+        const expiration = fromUnixTime(exp!);
+        const isExpired = isBefore(expiration, new Date());
+
+        if (!isExpired) return req;
+
+        const accessToken = await refreshToken(refresh);
+        req.headers.Authorization = `Bearer ${accessToken}`;
+
+        return req;
     },
     (error) => Promise.reject(error)
 );
