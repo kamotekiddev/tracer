31463f11ed9ece76c6a94cd2f8eaa8eb8265cdbd
kamotedev <kamotekiddev@gmail.com>
2024-05-28 14:00:00 +0800

    [FEATURE] Implement registration

---
+++ components/Loading/FullScreenLoading.tsx
@@ -0,0 +1,18 @@
+import { RotatingLines } from "react-loader-spinner";
+
+function FullScreenLoading() {
+    return (
+        <div className="fixed inset-0 grid place-items-center bg-neutral-950/10 backdrop-blur-sm">
+            <RotatingLines
+                visible={true}
+                width="50"
+                strokeColor="black"
+                strokeWidth="5"
+                animationDuration="0.75"
+                ariaLabel="rotating-lines-loading"
+            />
+        </div>
+    );
+}
+
+export default FullScreenLoading;
--- features/auth/authService.ts
+++ features/auth/authService.ts
@@ -1,8 +1,12 @@
 import client from "../services/api";
 import { LoginSchemaType } from "./login/validation";
+import { RegisterSchemaType } from "./register/validation";
 
 export const login = (credential: LoginSchemaType) =>
     client.post("/auth/login", credential);
 
 export const refresh = (refreshToken: string) =>
     client.patch("/auth/refresh", { refreshToken });
+
+export const register = (data: RegisterSchemaType) =>
+    client.post("/auth/register", data);
