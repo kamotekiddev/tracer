a766d13faaab7f1bf7a6566d96247c7b41ac9f94
kamotedev <kamotekiddev@gmail.com>
2024-10-14 21:01:11 +0800

    [FEATURE] Update issue assignee

--- features/projects/issue/IssueAssignee.tsx
+++ features/projects/issue/IssueAssignee.tsx
@@ -1,21 +1,61 @@
 import React, { useState } from "react";
-import { IssueWithProject } from "./issue.types";
-import MemberSelection from "./MemberSelection";
-import { User } from "@/features/interfaces/user";
 
+import { IssueWithProject, UpdateIssueEvent } from "./issue.types";
+import { User } from "@/features/interfaces/user";
+import { useUpdateIssue } from "./useIssueQuery";
+import { useToast } from "@/components/ui/use-toast";
+import { isAxiosError } from "axios";
+import { ErrorResponse } from "@/features/interfaces";
+import MemberSelection from "./MemberSelection";
 interface Props {
     issue: IssueWithProject;
     members: User[];
 }
 
 function IssueAssignee({ issue, members }: Props) {
+    const { toast } = useToast();
     const [assigneeId, setAssigneeId] = useState(issue.assigneeId);
+    const updateIssue = useUpdateIssue();
+
+    const handleChangeNewAssignee = async (newAssigneeId: string) => {
+        try {
+            setAssigneeId(newAssigneeId);
+            if (newAssigneeId === issue.assigneeId) return;
+
+            await updateIssue.mutateAsync({
+                issueId: issue.id,
+                updateEvent: UpdateIssueEvent.ASSIGNEE_CHANGE,
+                assigneeId: newAssigneeId,
+            });
+
+            toast({
+                title: "Issue Updated",
+                description: "Issue has been successfully updated.",
+            });
+        } catch (error) {
+            if (isAxiosError<ErrorResponse>(error)) {
+                const { message } = error.response?.data || {};
+                if (typeof message === "string")
+                    return toast({
+                        title: "Update Failed",
+                        description: message,
+                        variant: "destructive",
+                    });
+            }
+
+            toast({
+                title: "Update Failed",
+                description: "Something went wrong, Please try again alter",
+                variant: "destructive",
+            });
+        }
+    };
 
     return (
         <MemberSelection
             selectedMember={assigneeId || "unassigned"}
             members={members}
-            onSelectMember={setAssigneeId}
+            onSelectMember={handleChangeNewAssignee}
         />
     );
 }
