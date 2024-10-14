af788cfc91cd5b73fa6d3129dcf8917ce2b3221f
kamotedev <kamotekiddev@gmail.com>
2024-10-14 20:46:35 +0800

    [FEATURE] Update issue category

--- features/projects/issue/IssueCategory.tsx
+++ features/projects/issue/IssueCategory.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { IssueWithProject } from "./issue.types";
+import { IssueWithProject, UpdateIssueEvent } from "./issue.types";
 import { useGetProjectCategories } from "../useProjectQuery";
 import {
     Select,
@@ -9,20 +9,60 @@ import {
     SelectValue,
 } from "@/components/ui/select";
 import { ArchiveIcon } from "lucide-react";
+import { useUpdateIssue } from "./useIssueQuery";
+import { useToast } from "@/components/ui/use-toast";
+import { isAxiosError } from "axios";
+import { ErrorResponse } from "@/features/interfaces";
 
 interface Props {
     issue: IssueWithProject;
 }
 
 function IssueCategory({ issue }: Props) {
+    const [categoryId, setCategoryId] = useState(issue.categoryId);
+    const { toast } = useToast();
+
+    const updateIssue = useUpdateIssue();
     const { data: projectCategories } = useGetProjectCategories(
         issue.project.id,
     );
 
-    const [categoryId, setCategoryId] = useState(issue.categoryId);
+    const handleSelectNewCategory = async (newCategoryId: string) => {
+        try {
+            setCategoryId(newCategoryId);
+            if (newCategoryId === issue.categoryId) return;
+
+            await updateIssue.mutateAsync({
+                issueId: issue.id,
+                updateEvent: UpdateIssueEvent.CATEGORY_CHANGE,
+                categoryId: newCategoryId,
+            });
+
+            toast({
+                title: "Issue Updated",
+                description: "Issue has been successfully updated.",
+            });
+        } catch (error) {
+            if (isAxiosError<ErrorResponse>(error)) {
+                const { message } = error.response?.data || {};
+                if (typeof message === "string")
+                    return toast({
+                        title: "Update Failed",
+                        description: message,
+                        variant: "destructive",
+                    });
+            }
+
+            toast({
+                title: "Update Failed",
+                description: "Something went wrong, Please try again alter",
+                variant: "destructive",
+            });
+        }
+    };
 
     return (
-        <Select value={categoryId} onValueChange={setCategoryId}>
+        <Select value={categoryId} onValueChange={handleSelectNewCategory}>
             <SelectTrigger>
                 <SelectValue placeholder="Select Category" />
             </SelectTrigger>
